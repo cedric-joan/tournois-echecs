db5781b1a66066e493c9d96faf33a20289ffc334
cedric-joan <95696368+cedric-joan@users.noreply.github.com>
2022-12-20 21:57:26 +0100

    Mise à jour du module main

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-# from models.player import Player
-from controllers.menu_manager import MenuManager
 
-
-
-# def main():
-#     main_menu()
-
-
-
+from views.views_main_menu import ViewsMainMenu
 
 
 
 if __name__ == "__main__":
-    # users = Player.new_players()
-    # print(users)
-    menu = MenuManager.main_menu()
+    menu = ViewsMainMenu.main()
 
